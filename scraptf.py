e321c9e0b005059a7a429f3bc36b9c142855aabc
pablo castro <pcastr9@uic.edu>
2021-02-18 16:01:11 -0600

    Clean up and commenting

--- scraptf.py
+++ scraptf.py
@@ -1,12 +1,11 @@
-import urllib.request
-from bs4 import BeautifulSoup
-import browser_cookie3 as bc
-from numpy import average
-import requests
-import PySimpleGUI as sg
-import pandas as pd
+from bs4 import BeautifulSoup # Used for website scraping
+import browser_cookie3 as bc # Used for cookie access
+import requests # Gets webpages
+import PySimpleGUI as sg # GUI
+import pandas as pd # csv file reading
+from selenium import webdriver#
 
-def OrganizeCardData(cards):
+def OrganizeCardData(cards): # Organizes cards in list in format CARDTITLE, CARDPRICE(in refined), CARDGAMENAME
     organizedCards = [[]]
     for card in range(len(cards)):
         #print(cards[card])
@@ -35,11 +34,10 @@ def OrganizeCardData(cards):
         organizedCards[card].append(cardGameName)
         #organizedCards[card].append(cardImageURL)
         organizedCards.append([])
-        #print(cardGameName)
     organizedCards.pop()
     return organizedCards
 
-def getKeyPrice():
+def getKeyPrice(): # Returns current key price as float based on steam market price
     url = "https://steamcommunity.com/market/priceoverview/?appid=440&currency=1&market_hash_name=Mann%20Co.%20Supply%20Crate%20Key"
     page = requests.get(url, cookies=cookie)
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -51,9 +49,7 @@ def getKeyPrice():
     keyPrice = float(keyPrice)
     return keyPrice
 
-def getCardPrice(cardGame):
-    cardExel = pd.read_csv(r'STC_set_data.csv') # Read csv file with card prices
-
+def getCardPrice(cardGame, cardExel): # Estimates the card price (in USD) based on the csv file, RETURNS card price in USD (float)
     try:
         nameIndex = cardExel.loc[cardExel['Game'] == cardGame].index[0] # Get index of the row where the game is located
     except:
@@ -61,7 +57,7 @@ def getCardPrice(cardGame):
     cardPrice = cardExel["Card Avg"][nameIndex] # Get the actual price from the correct column using the row index from above
     return float(cardPrice)
 
-def calculateFee(realPrice):
+def calculateFee(realPrice): # Calculates how much the steam market listing fee is, RETURNS fee (float)
     steamTransactionFee = round(realPrice *.05, 2)
     if steamTransactionFee < 0.01:
         steamTransactionFee = 0.01
@@ -71,13 +67,13 @@ def calculateFee(realPrice):
     fee = steamTransactionFee + gameSpecificFee
     return fee
 
-def calculateProfit(realPrice, fee, refPrice):
+def calculateProfit(realPrice, fee, refPrice): # Estimates the profit based on the estimated steam market price, current key price, RETURNS profit(float, 2)
     oneRefBaseline = keyPrice / 51
     refPrice = oneRefBaseline * refPrice
     profit = (realPrice - fee) - refPrice
     return round(profit, 2) 
 
-# get raw data
+# get raw data of scrap.tf card page
 s = requests.session()
 url = "https://scrap.tf/cards/36"
 cookie = bc.chrome()
@@ -94,27 +90,19 @@ for child in cardHTML.children:
         pass
     else:
         rawCards.append(str(child))
-
 print("The card list has {} objects".format(len(rawCards)))
 cards = OrganizeCardData(rawCards)
-'''
-avgPrice = getCardPrice("Team Fortress 2")
-fee = calculateFee(avgPrice)
-profit = calculateProfit(avgPrice, fee, 0.44)
 
-'''
-for card in cards:
+cardExel = pd.read_csv(r'STC_set_data.csv') # Read csv file with card prices
+for card in cards: # calculate profit
     try:
-        avgPrice = getCardPrice(card[2])
+        avgPrice = getCardPrice(card[2], cardExel)
     except:
         break
     fee = calculateFee(avgPrice)
     profit = calculateProfit(avgPrice, fee, float(card[1]))
     card.append(profit)
-    #print(card[0] + ': ' + str(card[2]) + 'cost ' + card[1] + " ref and sells for " + str(avgPrice) + " on steam market, after fees we make " + str(profit) + " Profit")
-
-sortedCards = sorted(cards, key = lambda l:l[3])
-
+sortedCards = sorted(cards, key = lambda l:l[3]) # Sorts card list by most profitable
 for card in sortedCards:
     print(card)
 
