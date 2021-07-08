88ef7243be1d7c2d04d44e925d1c7f36f0a11d4a
pablo castro <Pablocastro079@gmail.com>
2021-07-07 20:12:30 -0500

    prices gotten directly from steam, prices stored in txt file for later use

--- scraptf.py
+++ scraptf.py
@@ -1,3 +1,4 @@
+from json.decoder import JSONDecodeError
 import time
 import json
 import pickle
@@ -101,7 +102,7 @@ def selectCardsScrapTF(driver): # Opens webpage with selenium and selects all th
     x = 0
     top20 = []
     for index, card in enumerate(mostProfitable):
-        if index > 20:
+        if index > 19:
             break
         else:
             top20.append(card)
@@ -193,19 +194,31 @@ def getAppID(cardGame, cardExel): # Get's AppId of game
 
 def getSpecificPrice(card, mostProfitableList, price_list): # Gets current price of a card
     cardName = card[0]
-    lock = threading.Lock()
     if cardName in price_list:
         current_lowest_price = float(price_list[cardName]['price'])
     else:
-        try:
-            market_hash_name = scrapy.get_card_hash_name(cardName)
-            URL_base = "https://steamcommunity.com/market/priceoverview/?appid=753&market_hash_name="
-            URL_full = URL_base + market_hash_name
-            page = requests.get(URL_full)
-            json_page = json.loads(page.content)
-            current_lowest_price = float(json_page['lowest_price'][1:])
+        with lock:
+            try:
+                market_hash_name = inventory[cardName]['internal_name'][:-1]
+                URL_base = "https://steamcommunity.com/market/priceoverview/?appid=753&market_hash_name="
+                URL_full = URL_base + market_hash_name + '-' + cardName
+                page = requests.get(URL_full)
+                time.sleep(10)
+                if page.status_code != 200:
+                    print(f"ERROR CODE {page.status_code}")
+                    log.write(f"ERROR CODE {page.status_code};")
+                    log.write("card: " + cardName + ";")
+                    log.write(" appID: " + card[2])
+                    log.write("\n")
+                    log.write("\n")
+                    return
 
-            with lock:
+                json_page = json.loads(page.content)
+                if 'lowest_price' in json_page:
+                    current_lowest_price = float(json_page['lowest_price'][1:])
+                else:
+                    current_lowest_price = 0.0
+
                 with open("price_list.txt", 'a', encoding="utf-8") as file_in:
                     file_in.write('{' + str(current_lowest_price) + ';')
                     file_in.write(cardName + ';')
@@ -214,36 +227,22 @@ def getSpecificPrice(card, mostProfitableList, price_list): # Gets current price
                     file_in.write('\n')
                     file_in.write('\n')
 
-        except IndexError:
-            current_lowest_price = 0.0
-        
-        #Too many requests
-        except TypeError: 
-            current_lowest_price = 0.0
+            except IndexError:
+                current_lowest_price = 0.0
 
-        except:
-            current_lowest_price = 0.0
+            # No market_hash_name found
+            except KeyError:
+                current_lowest_price = 0.0
+            
+            except JSONDecodeError:
+                current_lowest_price = 0.0
         
     fee = calculateFee(current_lowest_price)
     profit = calculateProfit(current_lowest_price, fee, float(card[1]))
 
-    with lock:
-        mostProfitableList.append(card)
+    mostProfitableList.append(card)
     index = mostProfitable.index(card)
     mostProfitableList[index][4] = profit
-    with lock:
-        try:
-            log.write("card: " + cardName + "---")
-            log.write("price: " + str(current_lowest_price) + '---')
-            log.write("hash_name: " + market_hash_name + '---')
-            log.write("URL: " + URL_full + '---')
-            log.write("appID: " + card[2])
-            log.write("\n")
-        except UnicodeEncodeError:
-            return
-
-        except UnboundLocalError:
-            return
 
 def save_cookies(requests_cookiejar, filename):
     with open(filename, 'wb') as f:
@@ -306,6 +305,18 @@ def load_inventory(file):
     
     return lines
 
+def update_inventory(inventory):
+    for item in scrapy.inventory.items:
+        if item in inventory:
+            continue
+        else:
+            with open("scrapy_inventory.txt", 'a', encoding="utf-8") as file_in:
+                # Write card name and internal names
+                file_in.write('{Card_name: ' + item.display_name + '; ')
+                file_in.write("Internal_name: " + item.tags[1]['internal_name'][4:] + '}')
+                file_in.write('\n')
+                file_in.write('\n')
+
 class MyClient(steam.Client):
     async def connect(self):  # on_events in a subclassed client don't need the @client.event decorator
         print("------------")
@@ -316,8 +327,10 @@ class MyClient(steam.Client):
         print("------------")
         user = await SteamID.from_url("https://steamcommunity.com/id/tf2scrap36")
         user = user.id64
-        user = await scrapy.fetch_user(76561198121097365)
+        user = await self.fetch_user(76561198121097365)
         self.inventory = await user.inventory(steam.STEAM)
+        update_inventory()
+        '''
         with open("scrapy_inventory.txt", 'w', encoding="utf-8") as file_in:
             for item in self.inventory.items:
                 # Write card name and internal names
@@ -325,6 +338,7 @@ class MyClient(steam.Client):
                 file_in.write("Internal_name: " + item.tags[1]['internal_name'][4:] + '}')
                 file_in.write('\n')
                 file_in.write('\n')
+        '''
         print('got inventory')
 
     # Search inventory of scrapy and return hash_name of provided card
@@ -344,10 +358,10 @@ class MyClient(steam.Client):
         await trade.accept()
 
 # Initial login to steam
-scrapy = MyClient()
-scrapy.run("metalyorkie", "0hBhG2ZtPK4O", ) #shared_secret="vaOhCJKJzWiu3oTRUbG+cCo5eOk=" metal079
+#scrapy = MyClient()
+#scrapy.run("metalyorkie", "0hBhG2ZtPK4O", ) #shared_secret="vaOhCJKJzWiu3oTRUbG+cCo5eOk=" metal079
 total_profit = 0.0 # Initial profit is 0
-#inventory = load_inventory('scrapy_inventory')
+inventory = load_inventory('scrapy_inventory.txt')
 while(True):
     # get raw data of scrap.tf card page
     url = "https://scrap.tf/cards/36"
@@ -364,10 +378,10 @@ while(True):
     driver = webdriver.Chrome(options=chrome_options)
     cookies = load_cookies("cookies.pkl")
     driver.get("https://scrap.tf/")
+    #pickle.dump( driver.get_cookies() , open("yorkie_cookies.pkl","wb")) # Dumps cookies
     for cookie in cookies:
         driver.add_cookie(cookie)
     driver.get("https://scrap.tf/cards/36")
-    time.sleep(10)
     page = driver.page_source
     
     # Organize cards
@@ -414,10 +428,11 @@ while(True):
     time_start = time.time()
     threads = []
     log = open("thread_debug_log.txt", "w")
-    log.write("-------")
+    log.write("")
     log.close()
     log = open("thread_debug_log.txt", "a", encoding="utf-8")
     price_list = load_price_list("price_list.txt")
+    lock = threading.Lock()
     for index, card in enumerate(sortedCards):
         price_request = threading.Thread(target=getSpecificPrice, args=(card, mostProfitable, price_list))
         price_request.start()
